4ed561062b255f00ef86e0e350ec687d51e02fb2
piwik-la <admin@piwik.la>
2014-07-27 01:35:45 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#https://github.com/piwikla/da
+
+#Update
+sudo yum update
+
+#pre-install
+yum install wget gcc gcc-c++ flex bison make bind bind-libs bind-utils openssl openssl-devel perl quota libaio libcom_err-devel libcurl-devel gd zlib-devel zip unzip libcap-devel cronie bzip2 db4-devel cyrus-sasl-devel perl-ExtUtils-Embed autoconf automake libtool which patch psmisc net-tools
+
+#setup
+wget http://www.directadmin.com/setup.sh
+chmod 755 setup.sh
+./setup.sh
+
+cd /usr/local/directadmin/custombuild
+./build update
+./build secure_php
+
+cd /usr/local/directadmin/custombuild
+./build update
+./build all d
+
+cp -f /usr/share/mysql/my-huge.cnf /etc/my.cnf

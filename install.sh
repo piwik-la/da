25706d413583e69d722ba09a3c3dd0bc9a8b88f5
piwik-la <admin@piwik.la>
2014-08-05 22:02:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,14 @@
 #https://github.com/piwikla/da
 
 #Update
-sudo yum update
+yum update
 
 #pre-install
 yum install wget gcc gcc-c++ flex bison make bind bind-libs bind-utils openssl openssl-devel perl quota libaio libcom_err-devel libcurl-devel gd zlib-devel zip unzip libcap-devel cronie bzip2 db4-devel cyrus-sasl-devel perl-ExtUtils-Embed autoconf automake libtool which patch psmisc net-tools
+
+#screen mode
+yum install screen
+screen -S da
 
 #setup
 wget http://www.directadmin.com/setup.sh
